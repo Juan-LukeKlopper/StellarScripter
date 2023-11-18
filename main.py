6686b4441a35d54af5ddbc23a4ec446dde7cd7e0
Juan-LukeKlopper <jlklopper@protonmail.com>
2023-11-18 18:03:38 +0200

    feat: take input on what blog to genereate

--- main.py
+++ main.py
@@ -5,23 +5,39 @@ from langchain.llms import Ollama
 from langchain.prompts import PromptTemplate
 
 llm = Ollama(model="mistral-openorca", 
-            #callback_manager = CallbackManager([StreamingStdOutCallbackHandler()]),
+            callback_manager = CallbackManager([StreamingStdOutCallbackHandler()]),
             temperature=0.9,
              )
 
 pre_prompt = "You are a respected industry leader in writing and blogs with indepth knowledge of all factors including tone and storytelling, "
-brainstorm_template = str(pre_prompt) + "Please brainstorm and create 10 ideas for a blog post around the topic: {topic}?"
-
-prompt = PromptTemplate(
+brainstorm_template = str(pre_prompt) + "Please brainstorm and create 10 ideas for a short to medium length blog post around the topic: {topic}?"
+brainstorm_prompt = PromptTemplate(
     input_variables=["topic"],
     template=brainstorm_template,
 )
 
-chain = LLMChain(llm=llm, 
-                 prompt=prompt,
+brainstorm_chain = LLMChain(llm=llm, 
+                 prompt=brainstorm_prompt,
                  verbose=False)
 
 user_input = input("What do you want to write a blog about: ")
 
 # Run the chain only specifying the input variable.
-print(chain.run(user_input))
+brainstorm_output = brainstorm_chain.run(user_input)
+#print(brainstorm_output)
+
+introduction_template = str(pre_prompt) + "Please output only an introduction about idea number {number} from the following list " + brainstorm_output + " Don't respond with any text othet than the introduction "
+
+introduction_prompt = PromptTemplate(
+    input_variables=["number"],
+    template=introduction_template
+)
+
+introduction_chain = LLMChain(llm=llm,
+                    prompt=introduction_prompt,
+                    verbose=False)
+
+choice_input = int(input("\n\nNumber to generate: "))
+print("\n\nHere is the introduction's first draft: \n\n\n")
+
+introduction_output = introduction_chain.run(choice_input)
